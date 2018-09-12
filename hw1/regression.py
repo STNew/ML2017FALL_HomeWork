fd32c9f9588915d445e1fa01f5825acd7a8e03c8
test-1 <ttt>
2018-09-12 20:32:34 +0800

    hw1_0

--- hw1/regression.py
+++ hw1/regression.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import scipy as sc
 import csv
+import matplotlib.pyplot as plt
+import matplotlib 
+import random 
 
 
 
@@ -9,3 +12,45 @@ x_data = [ 338.,  333.,  328. , 207. , 226.  , 25. , 179. ,  60. , 208.,  606.]
 y_data = [  640.  , 633. ,  619.  , 393.  , 428. ,   27.  , 193.  ,  66. ,  226. , 1591.]
 
 x=np.arange(-200,-100,1)
+y=np.arange(-5,5,0.1)
+z=np.zeros((len(x),len(y)))
+X,Y=np.meshgrid(x,y)
+for i in range(len(x)):
+    for j in range(len(y)):
+        b=x[i]
+        w=y[i]
+        z[i][j]=0
+        for n in range(len(x_data)):
+            z[i][j]=z[i][j]+(y_data[n]-w*x_data[n]-b)**2
+        z[i][j]/=len(x_data)
+
+
+b=-120
+w=-4
+lr=0.00001
+iterration=10000
+b_history=[b]
+w_histort=[w]
+
+for i in range(iterration):
+    b_grad=0
+    w_grad=0
+    for n in range(len(x_data)):
+        b_grad=b_grad-2*(y_data[n]-w*x_data[n]-b)
+        w_grad=w_grad-2*(y_data[n]-w*x_data[n]-b)*x_data[n]
+        b=b-lr*b_grad
+        w=w-lr*w_grad
+    b_history.append(b)
+    w_histort.append(w)
+for n in range(len(x_data)):
+    z=z+(y_data[n]-w*x_data[n]-b)**2
+print(x)
+
+plt.contourf(x,y,z,50,alpha=0.5,cmap=plt.get_cmap('jet'))
+plt.plot([-188.4],[2.67],'x',ms=12,lw=1.5,color='orange')
+plt.plot(b_history,w_histort,'o-',ms=3,lw=1.5,color='black')
+plt.xlim(-200,-100)
+plt.ylim(-5,5)
+plt.xlabel(r'$b$',fontsize=16)
+plt.ylabel(r'$w$',frozenset=16)
+plt.show()
---
+++ hw1/test.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# x_data = [ 338.,  333.,  328. , 207. , 226.  , 25. , 179. ,  60. , 208.,  606.]
+# y_data = [  640.  , 633. ,  619.  , 393.  , 428. ,   27.  , 193.  ,  66. ,  226. , 1591.]
+# x_np=np.array(x_data)
+# y_np=np.array(y_data)
+
+
+# z=x_np+y_np
+# print(z)
+a=100
+z=[100]
+z[0]=11
+print(len(z))
+print(z[0])
