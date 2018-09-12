b59aa4d8f3a4d56541bac31264ddee887d2d706d
test-1 <ttt>
2018-09-12 19:02:28 +0800

    git add .

---
+++ hw1/regression.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import scipy as sc
+import csv
+
+
+
+x_data = [ 338.,  333.,  328. , 207. , 226.  , 25. , 179. ,  60. , 208.,  606.]
+y_data = [  640.  , 633. ,  619.  , 393.  , 428. ,   27.  , 193.  ,  66. ,  226. , 1591.]
+
+x=np.arange(-200,-100,1)
